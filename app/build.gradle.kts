f5a67b68c33e3e99bbf3fa4f10097907d9afb535
baruiavijit72-sys <baruiavijit72@gmail.com>
2026-09-09 15:56:55 +0530

    Change applicationId to 'com.auramusic.app'

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
   compileSdk = 35
 
   defaultConfig {
-    applicationId = "com.example"
+    applicationId = "com.auramusic.app"
     minSdk = 24
     targetSdk = 35
     versionCode = 1
